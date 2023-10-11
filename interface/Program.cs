45ef317eba0caf04343ef4bf7daf33d85b66fb04
bobby <ande970s@stud.kea.dk>
2023-10-11 13:24:42 +0200

    :)

---
+++ interface/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Contracts;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Sinatra sinatra = new Sinatra();
+        Console.WriteLine($"Tonight featuring {sinatra.GetName()}"); // 1) Use getName
+        
+
+        
+        sinatra.Talk();
+        // 2) Uncomment this
+        sinatra.Sing();
+        
+        ISinger s = new Sinatra(); // 3) Assign New Sinatra instead of null
+        Console.WriteLine($"The singer is {s.GetName()}");
+        s.Sing();
+
+    }
+
+    interface ISinger
+    {
+        String GetName();
+        void Sing();
+
+    }
+
+    class Sinatra : ISinger// implement the interface
+    {
+        // Provide the interface implementation
+
+        public void Talk()
+        {
+
+            Console.WriteLine("Good evening Las Vegas");
+        }
+
+        public string GetName(){
+            Console.WriteLine();
+
+            return "Anders";
+        }
+
+        public void Sing(){
+            Console.WriteLine("Fly with me");
+        }
+
+        string ISinger.GetName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
